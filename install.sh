5120e22d3eaa633c6fbe7c6915412f1271f645dc
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2022-09-29 01:00:18 -0300

    feat: update foundation commands

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+export USER="antthoma" #Set your user here
 apt update
-apt install sudo cron vim libpam-pwquality -y
+apt install ufw sudo cron vim libpam-pwquality net-tools -y
+groupadd user42
+usermod -aG user42 ${USER}
+usermod -aG sudo ${USER}
